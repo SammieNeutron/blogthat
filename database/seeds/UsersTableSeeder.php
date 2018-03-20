bdfdbdc2538285f31e875a7ec8d01b018c5cb9df
Sammie Neutron <samsonadegboyega@gmail.com>
2018-03-20 19:16:48 +0100

    usertableseed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,16 +11,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // App\User::create([
-        // 	'name' => 'Samson Samuels',
-        // 	'email' => 'samsonadegboyega@gmail.com',
-        // 	'password' => bcrypt('password')
-        // ]);
-
         App\User::create([ 
         	'name' => 'admin',
         	'email' => 'admin@webcoupers.com',
         	'password' => bcrypt('password')
         ]);
+
     }
 }
